98888e2b6da61b558413e255ac14e14fffdfe041
Max Zavaleta <mzavaletav@gmail.com>
2022-12-20 19:39:28 -0500

    feat: add counter

--- parallel-proj/Program.cs
+++ parallel-proj/Program.cs
@@ -9,34 +9,40 @@ string asyncFlag  = Environment.GetEnvironmentVariable("TEST_ASYNC_FLAG") ?? "F"
 int sleepTimeMs  = int.Parse(Environment.GetEnvironmentVariable("SLEEP_TIME_MILISECONDS") ?? "1500");
 Console.WriteLine($"Start: Parameters: TEST_PARALLEL_FLAG->{parallelFlag} TEST_ASYNC_FLAG->{asyncFlag} SLEEP_TIME_MILISECONDS -> {sleepTimeMs}");
 
+int counter=0;
+
 MyProcess myProcess = new MyProcess(sleepTimeMs); 
 
 if (asyncFlag.Equals("T",StringComparison.CurrentCultureIgnoreCase)){
     if (parallelFlag.Equals("T",StringComparison.CurrentCultureIgnoreCase)){
         await Parallel.ForEachAsync(ids, async (item,CancellationToken)=>{
+            int x = Interlocked.Increment(ref counter);
             string result = await myProcess.executeAsync(item);
-            System.Console.WriteLine($"Result: {result} Elapsed {stopwatch.Elapsed}");   
+            System.Console.WriteLine($"{x:000}/{ids.Length:000} Result: {result} Elapsed {stopwatch.Elapsed}");   
         });
     }else{
         //Non parallel
         foreach (var item in ids)
         {
+            counter++;
             var result = await myProcess.executeAsync(item);
-            System.Console.WriteLine($"Result {result} elapsed {stopwatch.Elapsed}");
+            System.Console.WriteLine($"{counter:000}/{ids.Length:000} Result {result} elapsed {stopwatch.Elapsed}");
         }
     }
 }else{
     if (parallelFlag.Equals("T",StringComparison.CurrentCultureIgnoreCase)){
         Parallel.ForEach(ids, item=>{
+            int x = Interlocked.Increment(ref counter);
             var result = myProcess.execute(item);
-            System.Console.WriteLine($"Result {result} elapsed {stopwatch.Elapsed}");
+            System.Console.WriteLine($"{x:000}/{ids.Length:000} Result {result} elapsed {stopwatch.Elapsed}");
         });
     }else{
         //Non parallel
         foreach (var item in ids)
         {
+            counter++;
             var result = myProcess.execute(item);
-            System.Console.WriteLine($"Result {result} elapsed {stopwatch.Elapsed}");
+            System.Console.WriteLine($"{counter:000}/{ids.Length:000} Result {result} elapsed {stopwatch.Elapsed}");
         }
     }
 }
